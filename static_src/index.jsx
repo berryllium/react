553fc407a7882e52ae514855b8903a7d861e748e
berryllium <gorkundp@yandex.ru>
2019-11-24 22:38:43 +0300

    hw1 complete

--- static_src/index.jsx
+++ static_src/index.jsx
@@ -3,18 +3,24 @@ import ReactDOM from 'react-dom';
 
 
 let messages = ['Привет', 'Как дела?'];
-function send() {
-   alert('test')
+function addMessage() {
+   messages.push('Нормально')
+   ReactDOM.render(
+      <Chat messages={messages} />,
+      document.getElementById('root'),
+   );
 }
 
+const SendButton = () => <button onClick={addMessage}>Отправить</button>
 const MessageComponent = (props) => <div>{props.text}</div>;
-const SendButton = () => <button onclick="send()">Кнопка</button>;
-
-const MessageField = (props) => {
-   return props.messages.map(message => <MessageComponent text={message} />)
-};
+const MessageField = (props) => props.messages.map(message => <MessageComponent text={message} />);
+const Chat = (props) =>
+   <div>
+      <MessageField messages={props.messages} />
+      <SendButton />
+   </div>
 
 ReactDOM.render(
-   <MessageField messages={messages} />,
+   <Chat messages={messages} />,
    document.getElementById('root'),
 );
