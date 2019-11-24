9a7756d15f5692906f211eb0edd2588a7a41dde1
berryllium <gorkundp@yandex.ru>
2019-11-24 18:10:06 +0300

    init hw1

---
+++ static_src/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+let messages = ['Привет', 'Как дела?'];
+function send() {
+   alert('test')
+}
+
+const MessageComponent = (props) => <div>{props.text}</div>;
+const SendButton = () => <button onclick="send()">Кнопка</button>;
+
+const MessageField = (props) => {
+   return props.messages.map(message => <MessageComponent text={message} />)
+};
+
+ReactDOM.render(
+   <MessageField messages={messages} />,
+   document.getElementById('root'),
+);
