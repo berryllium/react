f25a8eed4e24f0cb1924b6ac9d60508563ba0b1c
berryllium <gorkund@yandex.ru>
2024-10-27 17:11:39 +0100

    redux persist store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import profileReducer from './profile'
 import chatReducer from './chats'
+import storage from 'redux-persist/lib/storage'
+import { persistReducer } from 'redux-persist'
+
+const reducers = combineReducers({
+  profile: profileReducer,
+  chats: chatReducer,
+})
+
+const persistConfig = {
+  key: 'root',
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export default configureStore({
-  reducer: {
-    profile: profileReducer,
-    chats: chatReducer,
-  },
+  reducer: persistedReducer,
 })
 
